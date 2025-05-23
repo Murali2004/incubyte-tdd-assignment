ecb61b9b461f65307c51dfa7cfe0a487d75bbf47
Murali2004 <mm3525953@gmail.com>
2025-05-23 22:40:14 +0530

    Return number itself for single number input

--- tests/addFunction.test.js
+++ tests/addFunction.test.js
@@ -1,4 +1,4 @@
 import {add} from "../src/addfunction.js"
-test('returns 0 for empty string', () => {
-  expect(add("")).toBe(0);
+test('returns number itself for one number', () => {
+  expect(add("1")).toBe(1);
 });
