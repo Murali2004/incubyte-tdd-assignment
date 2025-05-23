822d3c604fb90b03c1e441efbf26c9f4210ae8cd
Murali2004 <mm3525953@gmail.com>
2025-05-23 22:51:54 +0530

    Function now supports custom delimiters

--- src/addfunction.js
+++ src/addfunction.js
@@ -1,7 +1,15 @@
 export function add(numbers) {
   if (!numbers) return 0;
+
+  let delimiter = /,|\n/;
+  if (numbers.startsWith("//")) {
+    const parts = numbers.split("\n");
+    delimiter = new RegExp(parts[0].slice(2)); // custom delimiter
+    numbers = parts[1];
+  }
+
   return numbers
-    .split(/,|\n/)
+    .split(delimiter)
     .map(Number)
     .reduce((a, b) => a + b, 0);
 }
