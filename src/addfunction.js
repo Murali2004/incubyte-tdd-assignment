707212b98e48b18bc6c54c6071a7c111f7a2bf73
Murali2004 <mm3525953@gmail.com>
2025-05-23 22:49:17 +0530

    Refactor of add function and supports delimiters

--- src/addfunction.js
+++ src/addfunction.js
@@ -1,7 +1,7 @@
 export function add(numbers) {
-  if (numbers === "") return 0;
-  if (numbers.includes(",")) {
-    return numbers.split(",").map(Number).reduce((a, b) => a + b, 0);
-  }
-  return parseInt(numbers);
+  if (!numbers) return 0;
+  return numbers
+    .split(/,|\n/)
+    .map(Number)
+    .reduce((a, b) => a + b, 0);
 }
