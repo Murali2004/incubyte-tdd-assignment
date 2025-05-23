0466833cf61d6a062937a2ff7b58de223dd99f21
Murali2004 <mm3525953@gmail.com>
2025-05-23 22:53:17 +0530

    Fails because not implemented negative numbers and lists

--- tests/addFunction.test.js
+++ tests/addFunction.test.js
@@ -1,7 +1,8 @@
 import {add} from "../src/addfunction.js"
-test('supports custom delimiter', () => {
-  expect(add("//;\n1;2")).toBe(3);
+test('throws error for negative numbers', () => {
+  expect(() => add("1,-2,3,-4")).toThrow("negative numbers not allowed: -2,-4");
 });
+
 
 
 
